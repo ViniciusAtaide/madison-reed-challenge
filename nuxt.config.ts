238eaf14fabbf9250841d213c6edc4e7748dff7c
Vinicius Ataide <viniciusataid@gmail.com>
2025-10-13 14:46:05 -0300

    fix deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,91 +2,87 @@ export default defineNuxtConfig({
   app: {
     baseURL: '/',
     head: {
-      htmlAttrs: { lang: 'en' },
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      ]
-    }
+      htmlAttrs: { lang: "en" },
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
   },
 
   ssr: false,
-  
+
   nitro: {
     compressPublicAssets: true,
     minify: true,
     prerender: {
-      routes: ['/']
-    }
+      routes: ["/"],
+    },
   },
-  
-  css: [
-    '~/assets/css/theme.css'
-  ],
-  
+
+  css: ["~/assets/css/theme.css"],
+
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/image',
-    '@nuxt/test-utils',
-    '@nuxtjs/i18n',
-    '@pinia/nuxt',
-    '@nuxtjs/color-mode',
-    'nuxt-svgo'
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/image",
+    "@nuxt/test-utils",
+    "@nuxtjs/i18n",
+    "@pinia/nuxt",
+    "@nuxtjs/color-mode",
+    "nuxt-svgo",
   ],
-  
+
   svgo: {
     svgoConfig: {
       plugins: [
-        'preset-default',
+        "preset-default",
         {
-          name: 'removeDimensions',
-        }
-      ]
-   }
+          name: "removeDimensions",
+        },
+      ],
+    },
   },
 
   fonts: {
     families: [
-      { name: 'Inter', provider: 'google' },
-      { name: 'Poppins', provider: 'google' },
-      { name: 'system-ui', provider: 'none' }
-    ]
-  },
-  
-  colorMode: {
-    preference: 'light',
-    fallback: 'light', 
-    classSuffix: '', 
-    storageKey: 'mr-theme' 
-  },
-  
-  i18n: {
-    strategy: 'prefix_except_default',
-    locales: [
-      { 
-        code: 'en', 
-        name: 'English',
-        language: 'en-US',
-        file: 'en.json'
-      },
-      { 
-        code: 'pt-br', 
-        name: 'Português (Brasil)',
-        language: 'pt-BR',
-        file: 'pt-br.json'
-      }
+      { name: "Inter", provider: "google" },
+      { name: "Poppins", provider: "google" },
+      { name: "system-ui", provider: "none" },
     ],
-    langDir: 'locales',
-    defaultLocale: 'en',
+  },
+
+  colorMode: {
+    preference: "light",
+    fallback: "light",
+    classSuffix: "",
+    storageKey: "mr-theme",
+  },
+
+  i18n: {
+    strategy: "prefix_except_default",
+    locales: [
+      {
+        code: "en",
+        name: "English",
+        language: "en-US",
+        file: "en.json",
+      },
+      {
+        code: "pt-br",
+        name: "Português (Brasil)",
+        language: "pt-BR",
+        file: "pt-br.json",
+      },
+    ],
+    langDir: "locales",
+    defaultLocale: "en",
     detectBrowserLanguage: {
       useCookie: true,
-      cookieKey: 'i18n_redirected',
-      redirectOn: 'root',
+      cookieKey: "i18n_redirected",
+      redirectOn: "root",
       alwaysRedirect: false,
-      fallbackLocale: 'en'
+      fallbackLocale: "en",
     },
-    vueI18n: './i18n.config.ts'
-  }
-})
+    vueI18n: "./i18n.config.ts",
+  },
+});
